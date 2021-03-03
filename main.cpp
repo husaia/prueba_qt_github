049c6cbd4968fd820393278f491d79be2bb2d953
husaia <husaiab.flores@gmail.com>
2021-03-03 12:06:59 -0700

    Desplegado de un messaje box diciendo hola

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-
+#include <QMessageBox>
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -7,5 +7,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    QMessageBox msg;
+    msg.setText("hola mundo");
+    msg.exec();
     return a.exec();
+
 }
