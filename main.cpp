95c5dd3b5d4d9c3bcd28b63601380845f012963e
husaia <husaiab.flores@gmail.com>
2021-03-09 10:05:22 -0700

    agrege unos comentarios en main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    // vamos a calar el pull a ver si jala
     QMessageBox msg;
     msg.setText("alv no me sale lo que quiero hacer");
     msg.exec();
